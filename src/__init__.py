e3a2cecb40888a97f1b58d25f5964d5be61baea8
ijgnd <fooijgnd@barijgnd>
2020-06-26 06:29:16 +0200

    update gc

--- src/__init__.py
+++ src/__init__.py
@@ -15,7 +15,11 @@ from aqt import mw
 
 
 def gc(arg, fail=False):
-    return mw.addonManager.getConfig(__name__).get(arg, fail)
+    conf = mw.addonManager.getConfig(__name__)
+    if conf:
+        return conf.get(arg, fail)
+    else:
+        return fail
 
 
 def getUserSettingsAndMaybeWarn():
